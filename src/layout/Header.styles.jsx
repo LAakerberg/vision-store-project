35ef8231fbe463b18c2032fce57f5d375dbd3288
Linus Aakerberg <44141432+LAakerberg@users.noreply.github.com>
2023-03-20 21:20:04 +0100

    Added header component styles

--- src/layout/Header.styles.jsx
+++ src/layout/Header.styles.jsx
@@ -33,11 +33,20 @@ export const HeaderCall = styled.div``;
 export const HeaderCart = styled.div``;
 
 export const HeaderMain = styled.div`
-height: 80px;
+min-height: 80px;
+display: flex;
+justify-content: center;
+`;
+
+export const HeaderMainContainer = styled.div`
+width: 80%;
 align-items: center;
 display: flex;
 flex-direction: row;
-`;
+@media (max-width: 450px) {
+    flex-direction: column;
+}
+`
 
 export const HeaderLogo = styled.div`
 font-family: Anton;
@@ -51,6 +60,9 @@ color: #206074;
 
 export const HeaderSearch = styled.div`
 flex: 1;
+justify-content: center;
+align-items: center;
+padding: 10px;
 `;
 
 export const HeaderBottom = styled.div`
@@ -60,6 +72,14 @@ background-color: #FFF;
 
 export const HeaderNavigation = styled.div`
 width: 100%;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
 background-color: #FFF;
 filter: drop-shadow(0px 15px 10px #0000005a);
+`;
+
+export const HeaderNavigationContainer = styled.div`
+width: 80%;
 `;
